20d04183bfaafefaf40322c84fd34c924da050b2
nathaniel-mohr <strikerxxx22@comcast.net>
2021-02-25 11:20:57 -0800

    Creating UI

---
+++ Assets/TaskDescriptionDisplay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskDescriptionDisplay : MonoBehaviour
+{
+    public Text TaskDescriptionText;
+    void Update()
+    {
+        TaskDescriptionText.text = "Testing";
+    }
+}
