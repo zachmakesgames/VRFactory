f73c54439a5c7880080af389bb44b3d7835bdd53
nathaniel-mohr <strikerxxx22@comcast.net>
2021-05-21 01:49:41 -0700

    Adding todo list

--- Assets/TaskDescriptionDisplay.cs
+++ Assets/TaskDescriptionDisplay.cs
@@ -8,18 +8,18 @@ public class TaskDescriptionDisplay : MonoBehaviour
     public Text TaskDescriptionText;
 
     private string[] TaskDescriptions = {
-        "- Go to the board storage area.\n" +
+        "\n\n- Go to the board storage area.\n" +
         "- Pick up a band board and carry it over to the building table and place it\n" +
         "- Repeat the same thing for the 2nd band board.",
 
-        "- Go to the board storage area.\n" +
+        "\n\n- Go to the board storage area.\n" +
         "- Pick up a rim joist and carry it over to the building table and place it\n" +
         "- Repeat the same thing for the 2nd band board.",
 
         "- Pick up joist hangers and start installing them one by one by nailing them to the band board.\n" +
         "- Once the joist hangers are installed, pick up a floor joist and install it.",
         
-        "- Pick up a joist from the board storage area.\n" +
+        "\n\n- Pick up a joist from the board storage area.\n" +
         "- Start with one end (rim joist) and continue with the floor joists\n" +
         "- Lay joists on joist hangers and secure to joist hanger."
     };
